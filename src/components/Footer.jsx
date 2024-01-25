f25275d16e151b99a730e23eab73d21ff21dabce
EA Estrella~ <ericaangeline.estrella.m@bulsu.edu.ph>
2024-01-25 19:17:42 +0800

    color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,15 @@ const Footer = () => {
     <div className="footer ">
         <p className="title-text text-center text-light ">Follow Us on:</p>
         <div className="footer__links p-2 ">
-        <a href="/link1" className="footer__link para-text text-center ">
-        <i class="bi bi-instagram text-light me-2 " style={{ fontSize: '1rem' }}></i>@reve_esthetique</a>
-        <a href="/link2" className="footer__link para-text text-center">
-        <i class="bi bi-facebook me-2 " style={{ fontSize: '1rem' }}></i>Rêve Esthétique</a>
-        <a href="/link3" className="footer__link para-text text-center">
-        <i class="bi bi-tiktok me-2" style={{ fontSize: '1rem' }}></i>@reve_esthetique</a>
+        <a href="https://www.instagram.com/reve_esthetique/" className="footer__link para-text text-center text-light ">
+        <i class="bi bi-instagram text-light me-2 " style={{ fontSize: '1rem' }}></i>@reve_esthetique
+        </a>
+        <a href="https://www.facebook.com/reve.esthetique.MMXXIII" className="footer__link para-text text-center text-light">
+        <i class="bi bi-facebook me-2 " style={{ fontSize: '1rem' }}></i>Rêve Esthétique
+        </a>
+        <a href="https://www.tiktok.com/@reve_esthetique" className="footer__link para-text text-center text-light">
+        <i class="bi bi-tiktok me-2" style={{ fontSize: '1rem' }}></i>@reve_esthetique
+        </a>
       </div>
       <div className="gmail-div text-center para-text text-light mt-2">
         <i class="bi bi-envelope me-2"></i> reve.esthetique.ph@gmail.com
